bd7050bf78ee191d8b64c7a95c05e0ec62e07a90
PedroHeitor12567 <pedroheitorrdac@gmail.com>
2026-06-07 10:47:54 -0300

    refactor: corrigindo o cors

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from src.infrastructure.controllers.dosimetria_controller import router as dosimetria_router
 
 app = FastAPI(
@@ -9,7 +11,13 @@ app = FastAPI(
 
 app.include_router(dosimetria_router)
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/health")
 def health():
     return {"status": "ok"}
